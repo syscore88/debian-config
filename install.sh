7e614d36def4aaae8bca4ead8e819b287cec8b6b
Bartosz Szczeciński <54596452+syscore88@users.noreply.github.com>
2026-09-11 15:09:03 +0200

    update

--- install.sh
+++ install.sh
@@ -419,6 +419,14 @@ fi
 shopt -u nullglob
 rm -rf "$DEB_DIR"
 
+APPLICATIONS_DIR="$HOME/.local/share/applications"
+for shortcut in \
+    "gay.pancake.lsfg-vk-ui.desktop" \
+    "io.github.eugeniosegala.mako.desktop" \
+    "io.github.eugeniosegala.mako.uninstaller.desktop"; do
+    rm -f "$APPLICATIONS_DIR/$shortcut"
+done
+
 # ==========================================================
 #  ETAP 3/3: KONFIGURACJA USŁUG, BOOTLOADERA I ŚRODOWISKA
 # ==========================================================
